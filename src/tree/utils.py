53ac08aa1c46efdcb02370b70ad5fec9116127af
LenaRibena <helenakeitum@gmail.com>
2025-01-21 15:26:45 +0100

    Added a function to ensure no output from Hydra when debugging code

--- src/tree/utils.py
+++ src/tree/utils.py
@@ -1,4 +1,31 @@
 import torch
+from ruamel.yaml import YAML
+
+
+def update_hydra_config(config_path: str):
+    """Function to update the hydra outputs in the configuration file.
+    If debug mode is enabled, the outputs folder from Hydra will not be created.
+    Otherwise, the outputs will be saved in the outputs directory.
+
+    Args:
+        config_path (str): the path to the configuration file
+    """
+    yaml = YAML()
+    yaml.preserve_quotes = True
+
+    with open(config_path, "r") as file:
+        config = yaml.load(file)
+
+    debug = config["debug"]
+
+    if debug is True:
+        config["hydra"] = {"run": {"dir": "."}, "output_subdir": None}
+    else:
+        config["hydra"] = {"run": {"dir": "./outputs"}}
+
+    with open(config_path, "w") as file:
+        yaml.dump(config, file)
+
 
 def equal_batch_size(batch: torch.Tensor) -> torch.Tensor:
     """Function to ensure equal batch sizes for each forward pass
@@ -21,6 +48,7 @@ def equal_batch_size(batch: torch.Tensor) -> torch.Tensor:
     batch = torch.stack(batch)
 
     return batch
+
 
 class EarlyStopper:
     def __init__(self, patience: int = 5, delta: float = 0):
