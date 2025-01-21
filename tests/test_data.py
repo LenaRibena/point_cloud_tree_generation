f016c3f61d5efbc235510c8bf74ecb519a7742f4
LenaRibena <helenakeitum@gmail.com>
2025-01-21 12:22:57 +0100

    Update to test data for PCTreeDataset

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,9 @@
 from torch.utils.data import Dataset
 
-from tree.data import MyDataset
+from tree.data import PCTreeDataset
 
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
+    dataset = PCTreeDataset("data/raw/urban_tree_dataset")
     assert isinstance(dataset, Dataset)
