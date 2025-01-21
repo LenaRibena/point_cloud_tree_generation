9c7ac5c6d13396d82d54783423d5ba8090d7f542
LenaRibena <helenakeitum@gmail.com>
2025-01-21 12:22:25 +0100

    Traing script made compatible with urban tree dataset. Batch-size matching through random downsampling

--- src/tree/train.py
+++ src/tree/train.py
@@ -7,66 +7,54 @@ import torch
 import torch.utils.tensorboard
 from torch.utils.data import DataLoader
 from torch.nn.utils import clip_grad_norm_
-from tqdm.auto import tqdm
+from tqdm import tqdm
 
 from tree.data import PCTreeDataset
+from tree.evaluate import compute_all_metrics, jsd_between_point_cloud_sets
 from tree.models.common import get_linear_scheduler
 from tree.models.vae_gaussian import GaussianVAE
 from tree.models.vae_flow import FlowVAE
 from tree.models.flow import add_spectral_norm, spectral_norm_power_iteration
-from tree.evaluate import compute_all_metrics, jsd_between_point_cloud_sets
+from tree.utils import EarlyStopper
 
 import pdb
 
-logger = logging.getLogger()
-
-# Datasets and loaders
-logger.info('Loading datasets...')
-
 PROJECT_PATH = os.getcwd()
 
-def custom_collate_fn(batch):
-    # Find the smallest number of points in the batch
-    min_points = min(item.shape[0] for item in batch)
-    
-    # Truncate each point cloud to the smallest number of points
-    batch = [item[:min_points] for item in batch]
-    
-    # Stack the batch
-    batch = torch.stack(batch)
-    
-    return batch
+logger = logging.getLogger()
+logger.info('Loading datasets...')
 
-# Train, validate and test
 @hydra.main(version_base="1.2", config_path=os.path.join(PROJECT_PATH, "configs"), config_name='default_config')
 def train(args):
-    train_dset = PCTreeDataset(raw_data_path=os.path.join(PROJECT_PATH, "data", "raw", "urban_tree_dataset"),
-                               split='train'
+    # Create train, val and test loaders from the dataset
+    dset = PCTreeDataset(raw_data_path=os.path.join(PROJECT_PATH, *args.data_path),
+                               device=args.device,
+                               transform=args.transform
                                )
-    
-    train_iter = DataLoader(train_dset,
-                            batch_size=args.batch_size,
-                            num_workers=0,
-                            shuffle=True,
-                            collate_fn=custom_collate_fn
-                            )
 
-    # Model
+    train_iter, val_iter, test_iter = dset.get_train_val_test_loaders(
+        train_ratio=args.train_split,
+        val_ratio=args.val_split,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers
+    )
+
+    # Create the model
     logger.info('Building model...')
-    model = GaussianVAE(args).to(args.device)
+    model = GaussianVAE(args).to(dset.device)
     if args.model == 'gaussian':
-        model = GaussianVAE(args).to(args.device)
+        model = GaussianVAE(args).to(dset.device)
     elif args.model == 'flow':
-        model = FlowVAE(args).to(args.device)
+        model = FlowVAE(args).to(dset.device)
     logger.info(repr(model))
     if args.spectral_norm:
         add_spectral_norm(model, logger=logger)
 
-    # Optimizer and scheduler
-    optimizer = torch.optim.Adam(model.parameters(), 
-                                 lr=args.lr, 
+    # Define optimizer and scheduler
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=args.lr,
                                  weight_decay=args.weight_decay)
-    
+
     # scheduler = get_linear_scheduler(
     #     optimizer,
     #     start_epoch=args.sched_start_epoch,
@@ -75,35 +63,92 @@ def train(args):
     #     end_lr=args.end_lr
     # )
 
-    # Load data
-    batch = next(iter(train_iter))
-    x = batch.to(args.device)
+    # Early stopping
+    early_stopper = EarlyStopper(patience=args.patience, delta=args.delta)
 
-    # TODO: TEMP SOLUTION
-    x = x[:, :5000, :]
-
-    # Reset grad and model state
-    optimizer.zero_grad()
+    # Training loop
+    logger.info('Start training...')
     model.train()
-    if args.spectral_norm:
-        spectral_norm_power_iteration(model, n_power_iterations=1)
+    for epoch in tqdm(range(args.epochs), desc='Epochs'):
+        train_loss = 0
 
-    # Forward
-    kl_weight = args.kl_weight
-    loss = model.get_loss(x, kl_weight=kl_weight) #, writer=writer, it=it
+        for i, batch in enumerate(train_iter):
+            if args.debug and i > 1:
+                break
 
-    # Backward and optimize
-    loss.backward()
-    orig_grad_norm = clip_grad_norm_(model.parameters(), args.max_grad_norm)
-    optimizer.step()
-    # scheduler.step()
+            x = batch.to(dset.device)
 
-    logger.info('[Train] Iter %04d | Loss %.6f | Grad %.4f | KLWeight %.4f' % (
-        loss.item(), orig_grad_norm, kl_weight
-    )) # it,
+            # Reset grad and model state
+            optimizer.zero_grad()
+            if args.spectral_norm:
+                spectral_norm_power_iteration(model, n_power_iterations=1)
+
+            # Define Kullback-Leibler weighing and compute loss
+            kl_weight = args.kl_weight
+            loss = model.get_loss(x, kl_weight=kl_weight) #, writer=writer, it=it
+            train_loss += loss.item() * x.size(0)
+
+            # Backward and optimize
+            loss.backward()
+            orig_grad_norm = clip_grad_norm_(model.parameters(), args.max_grad_norm)
+            optimizer.step()
+            # scheduler.step()
+
+            if i % 100 == 0:
+                logger.info('[Train] Epoch %i | Iter %04d | Loss %.6f | Grad %.4f | KLWeight %.4f' % (
+                    epoch, i, loss.item(), orig_grad_norm, kl_weight
+                ))
+
+        train_loss /= len(train_iter.dataset)
+
+        # Validation loop
+        model.eval()
+        val_loss = 0
+        for j, batch in enumerate(val_iter):
+            if args.debug and j > 1:
+                break
+
+            x = batch.to(dset.device)
+            with torch.no_grad():
+                val_loss += model.get_loss(x, kl_weight=kl_weight).item() * x.size(0)
+
+        val_loss /= len(val_iter.dataset)
+
+        logger.info('Epoch %i | [Train] Averaged loss %.6f | [Val] Averaged loss %.6f' % (epoch, train_loss, val_loss))
+
+        # Early stopping if validation loss does not improve
+        early_stopper(val_loss, model)
+        if early_stopper.early_stop:
+            logger.info('Early stopping...')
+
+            break
+
+    # Save the model
+    logger.info('Saving model...')
+    model_path = os.path.join(PROJECT_PATH, 'models', f'{args.model}_model.pth')
+    torch.save(early_stopper.best_model_state, model_path)
+
+    # Load the best model and test it on the test set
+    early_stopper.load_best_model(model)
+
+    logger.info("Testing the model on the test set...")
+    model.eval()
+    test_loss = 0
+    for k, batch in enumerate(test_iter):
+        if args.debug and k > 1:
+            break
+
+        x = batch.to(dset.device)
+        with torch.no_grad():
+            test_loss += model.get_loss(x, kl_weight=kl_weight).item() * x.size(0)
+
+    test_loss /= len(test_iter.dataset)
+    logger.info('[Test] Averaged loss %.6f' % test_loss)
+    logger.info("Training complete.")
+
 
 # def validate_inspect(args):
-#     z = torch.randn([args.num_samples, args.latent_dim]).to(args.device)
+#     z = torch.randn([args.num_samples, args.latent_dim]).to(dset.device)
 #     x = model.sample(z, args.sample_num_points, flexibility=args.flexibility) #, truncate_std=args.truncate_std)
 #     writer.add_mesh('val/pointcloud', x, global_step=it)
 #     writer.flush()
@@ -120,7 +165,7 @@ def train(args):
 #     gen_pcs = []
 #     for i in tqdm(range(0, math.ceil(args.test_size / args.val_batch_size)), 'Generate'):
 #         with torch.no_grad():
-#             z = torch.randn([args.val_batch_size, args.latent_dim]).to(args.device)
+#             z = torch.randn([args.val_batch_size, args.latent_dim]).to(dset.device)
 #             x = model.sample(z, args.sample_num_points, flexibility=args.flexibility)
 #             gen_pcs.append(x.detach().cpu())
 #     gen_pcs = torch.cat(gen_pcs, dim=0)[:args.test_size]
@@ -131,7 +176,7 @@ def train(args):
 #     # gen_pcs *= val_dset.stats['std']
 
 #     with torch.no_grad():
-#         results = compute_all_metrics(gen_pcs.to(args.device), ref_pcs.to(args.device), args.val_batch_size)
+#         results = compute_all_metrics(gen_pcs.to(dset.device), ref_pcs.to(dset.device), args.val_batch_size)
 #         results = {k:v.item() for k, v in results.items()}
 #         jsd = jsd_between_point_cloud_sets(gen_pcs.cpu().numpy(), ref_pcs.cpu().numpy())
 #         results['jsd'] = jsd
