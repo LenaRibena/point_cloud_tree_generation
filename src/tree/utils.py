08a5421faa30500d54d53b0db4e650ee666a33f0
LenaRibena <helenakeitum@gmail.com>
2025-01-21 16:45:17 +0100

    Added Wand handler to sync logs with wandb

--- src/tree/utils.py
+++ src/tree/utils.py
@@ -1,8 +1,11 @@
+import logging
+
 import torch
+import wandb
 from ruamel.yaml import YAML
 
 
-def update_hydra_config(config_path: str):
+def update_hydra_config(config_path: str) -> bool:
     """Function to update the hydra outputs in the configuration file.
     If debug mode is enabled, the outputs folder from Hydra will not be created.
     Otherwise, the outputs will be saved in the outputs directory.
@@ -26,6 +29,8 @@ def update_hydra_config(config_path: str):
     with open(config_path, "w") as file:
         yaml.dump(config, file)
 
+    return debug
+
 
 def equal_batch_size(batch: torch.Tensor) -> torch.Tensor:
     """Function to ensure equal batch sizes for each forward pass
@@ -48,6 +53,12 @@ def equal_batch_size(batch: torch.Tensor) -> torch.Tensor:
     batch = torch.stack(batch)
 
     return batch
+
+
+class WandbHandler(logging.Handler):
+    def emit(self, record):
+        log_entry = self.format(record)
+        wandb.log({"log": log_entry})
 
 
 class EarlyStopper:
