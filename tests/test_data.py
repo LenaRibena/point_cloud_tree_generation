b9105f31fa599cca90b558a40819a6a976d204ed
LenaRibena <helenakeitum@gmail.com>
2025-01-23 13:45:52 +0100

    Change unit test to only match three sample files

--- tests/test_data.py
+++ tests/test_data.py
@@ -11,15 +11,15 @@ _PATH_DATA = "data/processed"
 @pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="No processed data found.")
 def test_my_dataset():
     """Test the MyDataset class."""
-    N = 3746
+    N = 3
     dataset = PCTreeDataset(_PATH_DATA)
     assert isinstance(dataset, Dataset)
     assert len(dataset) == N, f"Incorrect dataset size. Expected {N}, got {len(dataset)}"
 
-    train, val, test = dataset.get_train_val_test_loaders(0.8, 0.1, 32, 4)
-    assert len(train) == 94
-    assert len(val) == 12
-    assert len(test) == 12
+    train, val, test = dataset.get_train_val_test_loaders(1 / 3, 1 / 3, 1, 0)
+    assert len(train) == 1
+    assert len(val) == 1
+    assert len(test) == 1
 
     count = 0
     for dataset in [train, val, test]:
