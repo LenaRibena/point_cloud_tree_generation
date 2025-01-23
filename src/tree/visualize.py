b4e4c80e606a8b25a89f80a56cf2e99d52112c85
JasonMerit <Jasonarom@gmail.com>
2025-01-23 17:02:01 +0100

    Extend make trees green per Helena request

--- src/tree/visualize.py
+++ src/tree/visualize.py
@@ -1,6 +1,8 @@
+import numpy as np
 import open3d as o3d
 
 
-def view_tree(points):
+def view_tree(points: np.ndarray) -> None:
     cloud = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points))
+    cloud.paint_uniform_color([0.1, 0.8, 0.2])
     o3d.visualization.draw_geometries([cloud])
